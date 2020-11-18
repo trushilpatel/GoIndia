19ad9ab323e460f29cea482fa9408cf77370e63a
Trushil Patel <trushilpatel9898@gmail.com>
2020-11-18 20:51:34 +0530

    removed compression feature

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,7 +1,4 @@
 const CompressionPlugin = require("compression-webpack-plugin")
 module.exports = {
-	chainWebpack: (config) => {
-		config.plugin("CompressionPlugin").use(CompressionPlugin)
-	},
 	transpileDependencies: ["vuetify"],
 }
