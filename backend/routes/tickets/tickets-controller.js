2845645ffd598dc7f011d2216b02abc5217a15d5
Trushil Patel <trushilpatel9898@gmail.com>
2020-11-18 16:03:33 +0530

    removed unnecessary Ticket code

--- backend/routes/tickets/tickets-controller.js
+++ backend/routes/tickets/tickets-controller.js
@@ -1,36 +1,24 @@
 const { Tickets } = require("../../db/sequelize")
 
-let totalTickets = 0
-
 async function getRandomTicket(req, res) {
-	if (totalTickets === 0) {
-		totalTickets = await getNumberOfTotalTickets()
-		console.log("TOTAL TICKETS", totalTickets)
-	}
 	res.json(
 		await Tickets.findOne({
 			where: {
-				id: Math.floor(Math.random() * totalTickets),
+				id: Math.floor(Math.random() * Tickets.count()),
 			},
 		})
 	)
 }
 
 async function saveTicket(req, res) {
-	console.log("SAVE TICKET", req.body)
 	try {
 		await Tickets.build(req.body).save()
-		totalTickets++
-		console.log("TOTAL TICKETS", totalTickets)
-    res.send("DONE")
+		res.send("DONE")
 	} catch (err) {
 		res.send("ERROR")
 	}
 }
 
-async function getNumberOfTotalTickets() {
-	return await Tickets.count()
-}
 module.exports = {
 	getRandomTicket,
 	saveTicket,
